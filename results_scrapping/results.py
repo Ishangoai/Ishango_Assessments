e8fec4261903814b64a38c72d9d10a73c1f66e75
Ricardo Segundo <Ricardosegundo@gmail.com>
2022-03-28 20:54:36 +0200

    branched to try new solutions

--- results_scrapping/results.py
+++ results_scrapping/results.py
@@ -1,35 +1,20 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-URL = 'https://coderbyte.com/'
-LOGIN_ROUTE = 'sl'
+# Start the session
+session = requests.Session()
 
-RESULTS_PATH = 'dashboard/ishangoai-nx1aa'
+# Create the payload
+payload = {'username':'userjzfw5pml', 
+          'password':'oliver0424'
+         }
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
-            'origin': 'https://coderbyte.com',
-            'referer': 'https://coderbyte.com/sl'}
+# Post the payload to the site to log in
+s = session.post("https://coderbyte.com/sl", data=payload)
 
-s = requests.session()
-test_cookies = s.get(URL + LOGIN_ROUTE).cookies
+# Navigate to the next page and scrape the data
+s = session.get('https://coderbyte.com/developers')
 
-csrf_token = {'CoderbyteSessionKey': 'nFWZPgGKivrJnifqBPy8'}
-
-login_payload = {
-    'username': 'oliver@ishango.ai',
-    'password': 'oliver0424',
-    'pageToken': 'a33a31beb913663277787b546e273d9305ebd735'
-}
-
-login_req = s.post(URL + LOGIN_ROUTE,
-                    headers=HEADERS,
-                    data=login_payload
-                    )
-
-print(login_req)
-
-cookies = login_req.cookies
-
-soup = bs(s.get(URL + RESULTS_PATH).text, 'html.parser')
+soup = bs(s.text, 'html.parser')
 
 print(soup)
