ee061012a61115834914b1f35bd5b77266e9d8dc
Oliver Angelil <oliverangelil@Olivers-MacBook-Air.local>
2022-04-02 10:53:30 +0200

    changed results path

--- results_scrapping/results.py
+++ results_scrapping/results.py
@@ -5,7 +5,7 @@ URL = 'https://coderbyte.com/'
 LOGIN_ROUTE = 'sl'
 REQUEST_URL = 'backend/requests/sl/login.php'
 
-RESULTS_PATH = 'dashboard/ishangoai-nx1aa'
+RESULTS_PATH = 'dashboard/ishangoai-nx1aa:data-science-as-ypd9gqutaz'
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
             'origin': 'https://coderbyte.com',
