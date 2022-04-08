e16c0f9d0204b99430c01c93f88a42a06559efc1
Oliver Angélil <olivermangelil@gmail.com>
2022-04-08 08:48:25 +0200

    3-feature-development-retrieve-candidate-test_result-data (#11)
    * added regex to search for relevant window of data, read json, convert to df, and save as .csv
    
    * changes to Ricardo's comments
    
    * added newline at end of file
    
    * showed Vera and Monika how to push changes to github
    
    * improved Token fetching regex, added docstring
    
    Co-authored-by: Oliver Angelil <oliverangelil@Olivers-MacBook-Air.local>

--- results_scrapping/results.py
+++ results_scrapping/results.py
@@ -1,24 +1,23 @@
+'''
+This script logs into the coderbyte website, scrapes coding results, and saves them to a .csv file.
+Because the coderbyte API costs $200/month, this script can save Ishango costs.
+'''
+
 import requests
-from bs4 import BeautifulSoup as bs
+import re
+import json
+import pandas as pd
 
 URL = 'https://coderbyte.com/'
 LOGIN_ROUTE = 'sl'
 REQUEST_URL = 'backend/requests/sl/login.php'
-
 RESULTS_PATH = 'dashboard/ishangoai-nx1aa:data-science-as-ypd9gqutaz'
-
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
-            'origin': 'https://coderbyte.com',
-            'referer': 'https://coderbyte.com/sl'}
 
 with requests.session() as s:
         
-    initial_soup = s.get(URL + LOGIN_ROUTE).content
-    pageToken = str(initial_soup,'utf-8').split(r'window.__pageToken = "')[1].split(r'";')[0]
+    initial_soup = s.get(URL + LOGIN_ROUTE).text
 
-    #soup = bs(initial_soup.text, 'html.parser')
-    #script = soup.find_all('script')
-
+    pageToken = re.search(r'window\.__pageToken = (.*?);', initial_soup).group(1).replace('"', '')
     login_payload = {
         'username': 'oliver@ishango.ai',
         'password': 'oliver0424',
@@ -29,17 +28,17 @@ with requests.session() as s:
                         data=login_payload
                         )
 
-    #print(login_req.content)
+    page = s.get(URL + RESULTS_PATH).text
 
-    middle_soup = login_req.headers
-    #print(middle_soup)
-    cookies = login_req.cookies
-    #print(cookies)
 
-    soup = bs(s.get(URL + RESULTS_PATH).text, 'html.parser')
+# fetch the display name
+assessment_details = re.search(r"window\.__org_assessmentDetails = (.*?);", page).group(1)
+display_name = json.loads(assessment_details)['display_name']
+display_name = display_name.replace(' ', '_').lower()
 
-    script = soup.find_all('script')
-    print(type(script))
-    print(len(script))
+# fetch coding results
+data = re.search(r"window\.__org_candidates = (.*?);", page).group(1)
+data  = json.loads(data)
+df = pd.json_normalize(data)
+df.to_csv(display_name + '.csv', index=False)
 
-    #print(script)
