79fb8d18f9b9905ccbaaf8ad728d5eb84933874a
Ricardo Segundo <40991487+RicSegundo@users.noreply.github.com>
2022-04-10 07:40:38 +0200

    Merging develop branch into main (#19)
    * changed results path
    
    * added parsing code
    
    * 3-feature-development-retrieve-candidate-test_result-data (#11)
    
    * added regex to search for relevant window of data, read json, convert to df, and save as .csv
    
    * changes to Ricardo's comments
    
    * added newline at end of file
    
    * showed Vera and Monika how to push changes to github
    
    * improved Token fetching regex, added docstring
    
    Co-authored-by: Oliver Angelil <oliverangelil@Olivers-MacBook-Air.local>
    
    * improved regex and added comment
    
    * added ability to scrape from multiple assessments. Using reduce function to union multiple dfs
    
    * minor readability improvement
    
    * added Python 3 shebang
    
    * Update results.py
    
    * added new file with the credentials and removed them from the main app file
    
    Co-authored-by: Oliver Angelil <oliverangelil@Olivers-MacBook-Air.local>
    Co-authored-by: Oliver Angélil <olivermangelil@gmail.com>
    Co-authored-by: Monika Pluta <45171001+monika-pluta@users.noreply.github.com>

--- results_scrapping/results.py
+++ results_scrapping/results.py
@@ -1,41 +1,53 @@
+#!/usr/bin/env python3
+'''
+This script logs into the coderbyte website, scrapes coding results, and saves them to a .csv file.
+Because the coderbyte API costs $200/month, this script can save Ishango costs.
+'''
+# Test
 import requests
-from bs4 import BeautifulSoup as bs
+import re
+import json
+import pandas as pd
+import credentials as C
+import functools
 
 URL = 'https://coderbyte.com/'
-LOGIN_ROUTE = 'sl'
-REQUEST_URL = 'backend/requests/sl/login.php'
-
-RESULTS_PATH = 'dashboard/ishangoai-nx1aa'
-
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
-            'origin': 'https://coderbyte.com',
-            'referer': 'https://coderbyte.com/sl'}
+LOGIN_PAGE = 'sl'
+BACKEND_LOGIN_PAGE = 'backend/requests/sl/login.php'
+ASSESSMENT_PAGES = [
+    'dashboard/ishangoai-nx1aa:data-science-as-ypd9gqutaz',
+    'dashboard/ishangoai-nx1aa:data-science-as-zddhq9gim6',
+    'dashboard/ishangoai-nx1aa:data-science-as-ms3m246vv3'
+]
 
 with requests.session() as s:
         
-    initial_soup = s.get(URL + LOGIN_ROUTE).content
-    pageToken = str(initial_soup,'utf-8').split(r'window.__pageToken = "')[1].split(r'";')[0]
+    initial_soup = s.get(URL + LOGIN_PAGE).text
 
-    #soup = bs(initial_soup.text, 'html.parser')
-    #script = soup.find_all('script')
+    # regex explanation: 
+    # search for all occurrences that match pattern within re.search
+    # (.*?) will match any content. It is still unclear how "?" helps
+    pageToken = re.search(r'window\.__pageToken = "(.*?)";', initial_soup).group(1)
 
     login_payload = {
-        'username': 'oliver@ishango.ai',
-        'password': 'oliver0424',
+        'username': C.Payload.username,
+        'password': C.Payload.password,
         'pageToken': pageToken
     }
 
-    login_req = s.post(URL + REQUEST_URL,
+    login_req = s.post(URL + BACKEND_LOGIN_PAGE,
                         data=login_payload
                         )
 
-    print(login_req.content)
+    # fetch coding results
+    dfs = []
+    for page in ASSESSMENT_PAGES:
+        response = s.get(URL + page).text
+        results = re.search(r"window\.__org_candidates = (.*?);", response).group(1)
+        results  = json.loads(results)
+        df = pd.json_normalize(results)
+        dfs.append(df)
 
-    middle_soup = login_req.headers
-    #print(middle_soup)
-    cookies = login_req.cookies
-    print(cookies)
+df_final = functools.reduce(lambda top,bottom: pd.concat([top,bottom]), dfs)
+df_final.to_csv('df_final.csv', index=False)
 
-    soup = bs(s.get(URL + RESULTS_PATH).text, 'html.parser')
-
-    #print(soup)
