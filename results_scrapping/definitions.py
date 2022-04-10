74691e1a0ec3f04dc95e853382c06ffbf41a8534
Ricardo Segundo <Ricardosegundo@gmail.com>
2022-04-10 23:35:25 +0200

    refactored code to be compliant with the reviews

--- results_scrapping/definitions.py
+++ results_scrapping/definitions.py
@@ -13,11 +13,11 @@ class Paths(str, enum.Enum):
     LOGIN_PAGE = 'sl'
     BACKEND_LOGIN_PAGE = 'backend/requests/sl/login.php'
 
-    # Added this variable the path to the public folder to be
-    # added by Oliver
+    # path where the file will be saved if other than the
+    # current folder
     destination_folder = ''
 
-    export_file = '2022_Spring_assessment_results.csv'
+    export_file = '2022_ghana_assessment_results.csv'
 
 
 class Assessments(List, enum.Enum):
@@ -25,7 +25,7 @@ class Assessments(List, enum.Enum):
     Placeholder for the assessment links
     """
 
-    SPRING_2022_ASSESSMENT_PAGES = [
+    GHANA_2022_ASSESSMENT_PAGES = [
     'dashboard/ishangoai-nx1aa:data-science-as-ypd9gqutaz',
     'dashboard/ishangoai-nx1aa:data-science-as-zddhq9gim6',
     'dashboard/ishangoai-nx1aa:data-science-as-ms3m246vv3'
--- results_scrapping/results.py
+++ results_scrapping/results.py
@@ -3,14 +3,12 @@
 This script logs into the coderbyte website, scrapes coding results, and saves them to a .csv file.
 Because the coderbyte API costs $200/month, this script can save Ishango costs.
 '''
-# Test
-import pandas as pd
 
 import definitions as D
 from tools import return_results
 
 # retrieve results for a list of assessments
-assessment_results = return_results(D.Assessments.SPRING_2022_ASSESSMENT_PAGES)
+assessment_results = return_results(D.Assessments.GHANA_2022_ASSESSMENT_PAGES)
 
 # Export results
 assessment_results.to_csv(D.Paths.export_file, index=False)
