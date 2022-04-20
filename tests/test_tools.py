434c32d79b88f460473ebc0aa376b94c3aad698f
Oliver Angelil <oliverangelil@Olivers-MacBook-Air.local>
2022-04-20 18:05:36 +0200

    removed import login

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,12 +1,11 @@
 import pytest
 import requests
 import results_scrapping.definitions as D
-from results_scrapping.tools import login
-# Testing the login process
-# Successful requests have status code between 200 and 299
 
 
 def test_tools_login():
+    # Testing the login process
+    # Successful requests have status code between 200 and 299
     s = requests.session()
     status_code = s.get(D.Paths.URL + D.Paths.LOGIN_PAGE).status_code
 
