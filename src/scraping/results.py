3cf158362340ee8f1173946632e0e67ec8345107
Ricardo Segundo <Ricardosegundo@gmail.com>
2022-05-09 22:26:57 +0200

    added the __name__ == __main__ part and corrected the test that was breaking on github

--- src/scraping/results.py
+++ src/scraping/results.py
@@ -6,11 +6,17 @@ Because the coderbyte API costs $200/month, this script can save Ishango costs.
 import scraping.definitions as D
 from scraping.tools import login, retrive_and_model_results, save_results
 
-# login into coderbyte and return the session
-session = login()
 
-# retrieve results for a list of assessments
-results = retrive_and_model_results(D.Assessments.ghana_2022_assessments, session)
+def extract_results() -> None:
+    # login into coderbyte and return the session
+    session = login()
 
-# save the resulting dataframe
-save_results(results, D.Paths.ghana_2022_export_path)
+    # retrieve results for a list of assessments
+    results = retrive_and_model_results(D.Assessments.ghana_2022_assessments, session)
+
+    # save the resulting dataframe
+    save_results(results, D.Paths.ghana_2022_export_path)
+
+
+if '__name__' == '__main__':
+    extract_results()
