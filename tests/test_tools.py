a55903a50f7ab9e511ab745911eb29ded0987374
Ricardo Segundo <Ricardosegundo@gmail.com>
2022-05-30 16:31:47 +0200

    added new unit test to test github actions credentials retrieval

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,5 +1,6 @@
 import requests
 import scraping.definitions as D
+import scraping.credentials as C
 
 
 def test_tools_login():
@@ -9,3 +10,13 @@ def test_tools_login():
     status_code = s.get(D.Paths.URL + D.Paths.LOGIN_PAGE).status_code
 
     assert 199 < status_code < 300, "Connection not established"
+
+
+def test_credentials_retrieval():
+    # Check if the credentials store as environmental variables
+    # can be retrieved (specially in the github actions environment)
+    login_user = C.Payload.username
+    login_pass = C.Payload.password
+
+    assert login_user is not None, "Username not found"
+    assert login_pass is not None, "Username not found"
