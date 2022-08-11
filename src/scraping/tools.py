f7fc439853fcb20132d03f19f5fdf30099c65e24
Oliver Angelil <olivermangelil@gmail.com>
2022-08-11 13:30:52 +0200

    .

--- src/scraping/tools.py
+++ src/scraping/tools.py
@@ -131,15 +131,11 @@ def pre_process_results(dataframe: pd.DataFrame) -> pd.DataFrame:
     """
 
     # Correct N/A and empty [] values
-    dataframe.replace(["N/A"], np.nan, regex=True, inplace=True)
-    
-    #dataframe["mc_answers"] = dataframe["mc_answers"].str.strip("[]").astype(object)
+    #dataframe.replace(["N/A"], np.nan, regex=True, inplace=True)
+    dataframe["mc_answers"] = dataframe["mc_answers"].str.strip("[]").astype(object)
 
     # automatically convert datetimes to datetime64. https://stackoverflow.com/a/41230801/5392289
     dataframe = dataframe.apply(lambda col: pd.to_datetime(col, errors='ignore') if col.dtypes == object else col, axis=0)
-
-    # transform columns into final dtypes
-    # dataframe = dataframe.astype(dtype=col_types)
 
     return dataframe
 
